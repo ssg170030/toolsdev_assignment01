16ab6cd8cffa1796aca213668217aba74b24e3ee
Sean Gardner <xseangx@gmail.com>
2020-06-08 20:57:57 -0500

    06/08/2020

--- src/gathernews.py
+++ src/gathernews.py
@@ -1,10 +1,32 @@
+
 #Description: Scrape and Summarize articles
  	#find artices online about animtaion.
+#pip install nltk 
+#pip install newspaper3k
+import nltk 
+from newspaper import Article
+#Get the article from Cartoon Brew
+#url is the variable for the article
+url = 'https://www.cartoonbrew.com/sponsored-by-enoben/the-callipeg-app-offers-an-intuitive-new-way-to-do-2d-animation-on-an-ipad-190845.html'
+article = Article(url)
 
-def  articles(word):
-	
+#Do some NLP
+article.download()
+article.parse()
+#nltk.download('punkt')
+article.nlp()
 
 
-keyinput = keyinput("Enter a word:")
-print(keyinput)
-print(articles(keyinput))
+#Get the authors
+article.authors
+
+#get publis datpye
+article.publish_date
+
+article.top_image
+
+#article text
+#
+
+
+#print(article.summary)
