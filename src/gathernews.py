bf78f00e2a86df953ee16354d943894c3309078f
Sean Gardner <xseangx@gmail.com>
2020-06-10 17:27:10 -0500

    "6/10/2020"

--- src/gathernews.py
+++ src/gathernews.py
@@ -1,31 +1,41 @@
-#Description: Scrape and Summarize articles
- 	#find artices online about animtaion.
-#pip install nltk 
-#pip install newspaper3k
-import nltk 
+# Description: Scrape and Summarize articles
+# find artices online about animtaion.
+# Get the article from Cartoon Brew
+
+
+import nltk
 from newspaper import Article
-#Get the article from Cartoon Brew
-#url is the variable for the article
+
+#url is a variable to store the article
 url = 'https://www.cartoonbrew.com/sponsored-by-enoben/the-callipeg-app-offers-an-intuitive-new-way-to-do-2d-animation-on-an-ipad-190845.html'
 article = Article(url)
 
-#Do some NLP
+#download to get the article
 article.download()
+
+#<!DOCTYPE HTML><html itemscope itemscope itemtype = "https://www.cartoonbrew.com/sponsored-by-enoben/the-callipeg-app-offers-an-intuitive-new-way-to-do-2d-animation-on-an-ipad-190845.html"
 article.parse()
-#nltk.download('punkt')
+nltk.download('punkt')
 article.nlp()
 
+article.html
 
-#Get the authors
+# Get the authors who wrote the article
 article.authors
 
-#get publis datpye
+# get publish date
 article.publish_date
 
+#finds the image and prints out a link to be shown on the Web
 article.top_image
 
-#article text
-#
+#collect the text
+article.text
 
+#article.summary
 
-#print(article.summary)
+#prints out the output to be shown in the code when gathernews.py is called upon
+#print(article.publish_date)
+#print(article.authors)
+print(article.authors)
+
