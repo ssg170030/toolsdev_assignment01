b12ba85066e96ce304b2881e106fc217ad002ad5
Sean Gardner <xseangx@gmail.com>
2020-06-12 10:10:26 -0500

    06/12/2020

--- src/gathernewsVer2.py
+++ src/gathernewsVer2.py
@@ -12,7 +12,7 @@ def extract_article(article):
 
 #runing as a stand alone
 #testing code works
-def extract_articles(news_site, iscached=False):
+def extract_articles(news_url, iscached=False):
     #Scans news site and extracts a list of content
     bna_news = newspaper.build(news_url, memoize_articles=iscached)
     for article in bna_news.articles:
@@ -23,5 +23,6 @@ def extract_articles(news_site, iscached=False):
 
 if __name__ == "__main__":
     news_sources = ["http://www.beforesandafters.com","https://www.cartoonbrew.com","https://www.fxguide.com/"]
-    for news_irl in news_sources:
+    for news_url in news_sources:
+        print(news_url)
         extract_articles(news_url)
